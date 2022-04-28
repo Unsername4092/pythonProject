940770d06000ac692a59011fa99aa845616683c5
Unsername4092 <qrudgns17@gmail.com>
2022-04-29 02:11:42 +0900

    upload

--- PE2/team1/ect.py
+++ PE2/team1/ect.py
@@ -1,19 +1,53 @@
-# <examples/doc_model_gaussian.py>
+import xml.etree.ElementTree as elemTree
 import matplotlib.pyplot as plt
-from numpy import exp, loadtxt, pi, sqrt
+import numpy as np
+from numpy import exp
+from sklearn.metrics import r2_score
 from lmfit import Model
-data = loadtxt('model1d_gauss.dat')
-x = data[:, 0]
-y = data[:, 1]
-def gaussian(x, amp, cen, wid):
-    """1-d gaussian: gaussian(x, amp, cen, wid)"""
-    return (amp / (sqrt(2*pi) * wid)) * exp(-(x-cen)**2 / (2*wid**2))
-gmodel = Model(gaussian)
-result = gmodel.fit(y, x=x, amp=5, cen=5, wid=1)
-print(result.fit_report())
-plt.plot(x, y, 'o')
-plt.plot(x, result.init_fit, '--', label='initial fit')
-plt.plot(x, result.best_fit, '-', label='best fit')
-plt.legend()
+data = elemTree.parse('LION1.xml')
+root = data.getroot()
+V = []
+for v in root.iter('Voltage'):
+    V.extend(list(map(float, v.text.split(','))))
+I = []
+for i in root.iter('Current'):
+    I.extend(list(map(float, i.text.split(','))))
+    I = list(map(abs, I))
+
+x = np.array(V[:])
+y = np.array(I[:])
+fit1 = np.polyfit(x, y, 12)
+fit1 = np.poly1d(fit1)
+
+def IV_fit(X, Is, Vt):
+    return (Is * (exp(X/Vt) - 1))    #
+
+model = Model(IV_fit)
+result = model.fit(y, X=x, Is=10**-15, Vt=0.026)
+
+def IVR(y):
+    global yhat, ybar
+    yhat = result.best_fit
+    ybar = np.sum(y)/len(y)
+    sse = np.sum((yhat - ybar) ** 2)
+    sst = np.sum((y - ybar) ** 2)
+    return sse/sst
+
+# 결정계수 구하는 다른 방법
+r2 = r2_score(I, result.best_fit)
+print(r2)
+
+plt.subplot(2, 2, 1)
+plt.plot(V, I, 'b.', label='data', markersize=8)
+plt.plot(x, result.best_fit, label='best_fit')
+plt.plot(x, result.best_fit, 'r-', label='R-squared ={}'.format(IVR(y)))
+plt.legend(loc = 'best')
+#plt.yscale('log')
+plt.title('I-V analysis', fontsize=18, fontweight='bold')
+plt.xlabel('Voltage[V]', fontsize=13)
+plt.ylabel('Current[A]', fontsize=13)
 plt.show()
-# <end examples/doc_model_gaussian.py>
+print(yhat-ybar)
+print(ybar)
+print(yhat)
+print(I)
--- PE2/team1/team1.py
+++ PE2/team1/team1.py
@@ -2,6 +2,8 @@ import xml.etree.ElementTree as elemTree
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import r2_score
+import pandas as pd
+from lmfit.models import ExpressionModel
 data = elemTree.parse('LION1.xml')
 root = data.getroot()
 # I-V graph
@@ -17,6 +19,7 @@ plt.title('I-V analysis', fontsize=18, fontweight='bold')   # Graph title
 plt.xlabel('Voltage (V)', fontsize=13)                # title of x-axis
 plt.ylabel('Current (A)', fontsize=13)                # title of y-axis
 plt.yscale('log')                                     # Determine the y-scale
+plt.yticks([10**i for i in range(-1,-11,-1)])       # y축의 간격을 설정
 plt.scatter(V, np.exp(np.log(np.abs(I))))           # Plot the Voltage and Current values.
 # Regression
 p = np.polyfit(V, np.abs(I), 12)       # 다항식계수 구하기
@@ -24,7 +27,9 @@ y = np.polyval(p, V)                  # fitting 한 값 데이터
 r2 = r2_score(np.abs(I), y)             # 결정계수
 print(r2)
 plt.plot(V, y, color='y', label='R squared {}'.format(r2))
-plt.legend(loc='best', ncol=2)
+print(I,V)
+
+plt.legend(loc='lower left', ncol=2)
 # Wavelength-Transmission graph
 L = []                      # Empty list for saving wavelength
 for n in root.iter('L'):    # Save the each wavelength data in the list
@@ -51,13 +56,33 @@ for sp in range(2, 5):         # sp - subplot
         plt.plot(L[-1], yr, color='black', linestyle='dashed', label='R squared = {}'.format(r2_r))
     if sp == 3:                        # 세번째 subplot 에는 기존 그래프에 fitting 된 reference 값을 빼준다.
         for r in range(len(L)-1):
-            IL2 = []                   # IL2 는 reference 를 뺀 값을 넣는다.
-            for i in range(len(IL[r])):
-                IL2.append(IL[r][i]-yr[i])
-            plt.plot(L[r], IL2, label=wls[r])
+            plt.plot(L[r], IL[r]-yr, label=wls[r])
     if sp == 4:                        # 네번째 subplot 에는 raw data 그래프를 그린다.
         for r in range(len(L)):
             plt.plot(L[r], IL[r], label=wls[r])
-    plt.legend(loc='best', ncol=2)
+    plt.legend(loc='lower left', ncol=2)
 plt.tight_layout()
-plt.show()                                              # Show the graphs
+plt.savefig('graph')
+#plt.show()                                              # Show the graphs
+TestSiteInfo=root.find('TestSiteInfo')
+dict={'Lot':[],'Wafer':[],'Mask':[],'TestSite':[],'Name':[],'Date':[],'Operator':[],'DieColumn':[],'DieRow':[],'AnalysisWavelength (nm)':[],'Rsq of Ref. spectrum (6th)':[] ,'Max transmission of Ref. spec. (dB)':[]}
+ref=IL[-1]
+print(type(yr),type(ref))
+AlignWavelength=next(root.iter('AlignWavelength'))
+Modulator=next(root.iter('Modulator'))
+for i in range(len(L)):
+    dict['Lot'].append(TestSiteInfo.attrib['Batch'])
+    dict['Wafer'].append(TestSiteInfo.attrib['Wafer'])
+    dict['Mask'].append(TestSiteInfo.attrib['Maskset'])
+    dict['TestSite'].append(TestSiteInfo.attrib['TestSite'])
+    dict['Name'].append(Modulator.attrib['Name'])
+    dict['Date'].append(root.attrib['CreationDate'])
+    dict['Operator'].append(root.attrib['Operator'])
+    dict['AnalysisWavelength (nm)'].append(AlignWavelength.text)
+    dict['Max transmission of Ref. spec. (dB)'].append(min(IL[i]-yr))
+
+    #dict['Rsq of Ref. spectrum (6th)'].append(r2_score(IL[i]-ref,IL[i]-yr))
+
+
+
+print(pd.DataFrame(dict))
